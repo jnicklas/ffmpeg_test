c52a56eda3e7d3581314344d660a3c897006c8ad
Jonas Nicklas and Kim Burgestrand <dev+jnicklas+burgestrand@elabs.se>
2012-12-06 18:31:15 +0100

    Don't load all this crap we don't need

--- test.c
+++ test.c
@@ -6,11 +6,7 @@
 
 #include <libavutil/opt.h>
 #include <libavcodec/avcodec.h>
-#include <libavutil/channel_layout.h>
 #include <libavutil/common.h>
-#include <libavutil/imgutils.h>
-#include <libavutil/mathematics.h>
-#include <libavutil/samplefmt.h>
 #include "lavfutils.h"
 
 #define INBUF_SIZE 4096
